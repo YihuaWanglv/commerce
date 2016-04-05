ee988cec1d913574d808318d91ba7cdf4d5163d8
yihuawanglv <wanglvyihua@gmail.com>
2016-04-05 18:45:30 +0800

    add web projects

---
+++ module_models/src/main/java/com/iyihua/commerce/remote/common/redis/RedisRemote.java
@@ -0,0 +1,55 @@
+package com.iyihua.commerce.remote.common.redis;
+
+import java.util.Set;
+
+public interface RedisRemote {
+	
+	public int expire = 0;
+	public int timeout = 0;
+
+	public byte[] get(byte[] key);
+
+	/**
+	 * set
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public byte[] set(byte[] key, byte[] value);
+
+	/**
+	 * set
+	 * 
+	 * @param key
+	 * @param value
+	 * @param expire
+	 * @return
+	 */
+	public byte[] set(byte[] key, byte[] value, int expire);
+
+	/**
+	 * del
+	 * 
+	 * @param key
+	 */
+	public void del(byte[] key);
+
+	/**
+	 * flush
+	 */
+	public void flushDB();
+
+	/**
+	 * size
+	 */
+	public Long dbSize();
+
+	/**
+	 * keys
+	 * 
+	 * @param regex
+	 * @return
+	 */
+	public Set<byte[]> keys(String pattern);
+}
---
+++ web_seller/src/test/java/com/iyihua/bootdemo/AppTest.java
@@ -0,0 +1,26 @@
+package com.iyihua.bootdemo;
+
+
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.iyihua.commerce.web.seller.App;
+
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+public class AppTest {
+	
+
+}
